f6cf143ca6eab201a20fc7e8ea8d2d8e04851b7b
Kieran Reid <kieranreid2612@gmail.com>
2026-03-07 20:00:03 +0000

    Changed std::cin to std::getline to get full command input, and to make echo function

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,28 @@
+#include <cstdint>
 #include <iostream>
 #include <string>
+
+std::string erase_command(std::string command, std::string built_in) {
+  std::int32_t length = built_in.length();
+  return command.erase(0, length + 1);
+}
+
+void echo(std::string command) {
+  std::string response = erase_command(command, "echo");
+  std::cout << response << "\n";
+}
 
 void repl() {
   std::string command;
 
   std::cout << "$ ";
 
-  std::cin >> command;
+  std::getline(std::cin, command);
 
-  if (command == "exit")
+  if (command.find("exit") == 0)
     exit(0);
+  if (command.find("echo") == 0)
+    echo(command);
   else
     std::cout << command << ": command not found\n";
 }
