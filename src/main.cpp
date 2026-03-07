ea585677cffc7cbe67e53d1365b8f5f525472970
Kieran Reid <kieranreid2612@gmail.com>
2026-03-07 19:28:41 +0000

    Added repl method to print $ then allow user input

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,24 @@
 #include <string>
 
 
-int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-
+void repl() {
   std::string command;
 
   std::cout << "$ ";
 
   std::cin >> command;
 
-  std::cout << command << ": command not found";
+  std::cout << command << ": command not found\n";
 }
+
+int main() {
+  // Flush after every std::cout / std:cerr
+  std::cout << std::unitbuf;
+  std::cerr << std::unitbuf;
+
+  while(1) {
+	repl();
+  }
+
+}
+
