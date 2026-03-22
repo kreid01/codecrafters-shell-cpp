7becb98f11e4035a12a9a24a1992ebd90c76565e
Kieran Reid <kieranreid2612@gmail.com>
2026-03-22 15:01:52 +0000

    Implemented in_array to check for type in command

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <cstdint>
 #include <iostream>
 #include <string>
+#include <vector>
+
+std::vector<std::string> built_ins = {"exit", "echo", "type"};
 
 std::string erase_command(std::string command, std::string built_in) {
   std::int32_t length = built_in.length();
@@ -12,6 +15,24 @@ void echo(std::string command) {
   std::cout << response << "\n";
 }
 
+bool in_array(const std::string &value, const std::vector<std::string> &array) {
+  return std::find(array.begin(), array.end(), value) != array.end();
+}
+
+void not_found(std::string command) {
+  std::cout << command << ": command not found\n";
+}
+
+void type(std::string command) {
+  std::string response = erase_command(command, "type");
+
+  if (in_array(response, built_ins)) {
+    std::cout << response << " is a shell builtin\n";
+  } else {
+    std::cout << response << ": not found\n";
+  }
+}
+
 void repl() {
   std::string command;
 
@@ -21,14 +42,15 @@ void repl() {
 
   if (command.find("exit") == 0)
     exit(0);
-  if (command.find("echo") == 0)
+  else if (command.find("echo") == 0)
     echo(command);
-  else
-    std::cout << command << ": command not found\n";
+  else if (command.find("type") == 0) {
+    type(command);
+  } else
+    not_found(command);
 }
 
 int main() {
-  // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
